2ee96e0dc97a7920719e41c01d945d2cddb74aa4
Pieter De Clercq <pieterdeclercq@outlook.com>
2015-12-04 20:40:36 +0100

    Added structure

--- index.php
+++ index.php
@@ -11,6 +11,41 @@ $theme = (date("H") < 6 || date("H") > 21) ? "darkly":"flatly";
 </head>
 <body>
 <div class="container">
+  <div class="row">
+    <div class="col-md-12" style="text-align:center"><h1>Pieter De Clercq</h1></div>
+  </div>
+  <div class="row">
+    <div class="col-md-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">
+          <h3 class="panel-title">Languages</h3>
+        </div>
+        <div class="panel-body">
+          TODO
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">
+          <h3 class="panel-title">Photostream</h3>
+        </div>
+        <div class="panel-body">
+          TODO
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">
+          <h3 class="panel-title">Contact information</h3>
+        </div>
+        <div class="panel-body">
+          TODO
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 <script type="text/javascript" src="assets/js/jquery.min.js"></script>
 <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
