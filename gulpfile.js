dd30c94efc2088c154af486d09935a41921e0514
Pieter De Clercq <pieterdeclercq@outlook.com>
2019-06-05 17:51:12 +0200

    Bump to 2019. Fixes #3.

--- gulpfile.js
+++ gulpfile.js
@@ -7,18 +7,17 @@ const fs = require('fs');
 const merge2 = require('merge2');
 const minify = require('gulp-clean-css');
 const order = require('gulp-order');
-const realFavicon = require('gulp-real-favicon');
 const sass = require('gulp-sass');
 const uglify = require('gulp-uglify');
 
 /* CSS */
-const cssCommon = ['assets/bower/font-awesome/css/font-awesome.min.css', 'assets/bower/devicon/css/devicon.min.css', 'assets/bower/devicon/devicon-colors.css', 'assets/css/src/*.sass', 'assets/css/src/*.scss'];
-const cssDaily = ['assets/bower/bootswatch/flatly/bootstrap.min.css'];
-const cssNightly = ['assets/bower/bootswatch/darkly/bootstrap.min.css'];
+const cssCommon = ['node_modules/font-awesome/css/font-awesome.min.css', 'node_modules/devicon/devicon.min.css', 'node_modules/devicon/devicon-colors.css', 'assets/css/src/*.sass', 'assets/css/src/*.scss'];
+const cssDaily = ['node_modules/bootswatch/dist/flatly/bootstrap.min.css'];
+const cssNightly = ['node_modules/bootswatch/dist/darkly/bootstrap.min.css'];
 const cssOut = 'assets/css';
 
 /* JS */
-const jsIn = ['assets/bower/jquery/dist/jquery.min.js'];
+const jsIn = ['node_modules/jquery/dist/jquery.min.js'];
 const jsOut = 'assets/js';
 
 /* COFFEE */
@@ -26,14 +25,14 @@ const coffeeIn = ['assets/js/src/*.coffee'];
 const coffeeOut = 'assets/js';
 
 /* FONTS */
-const fontsIn = ['assets/bower/font-awesome/fonts/*', 'assets/bower/devicon/fonts/*'];
+const fontsIn = ['node_modules/font-awesome/fonts/*', 'node_modules/devicon/fonts/*'];
 const fontsOut = 'assets/fonts';
 
 /* TASKS */
 gulp.task('coffee', function () {
     return gulp.src(coffeeIn)
         .pipe(coffee())
-        .pipe(babel({presets: ['env']}))
+        .pipe(babel())
         .pipe(concat('combined.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest(coffeeOut));
@@ -72,68 +71,6 @@ gulp.task('css-nightly', function () {
         .pipe(gulp.dest(cssOut));
 });
 
-gulp.task('css', ['css-common', 'css-daily', 'css-nightly'], function () {
-});
-
-gulp.task('favicon', function () {
-    realFavicon.generateFavicon({
-        masterPicture: 'favicon.png',
-        dest: '.',
-        iconsPath: '/',
-        design: {
-            ios: {
-                pictureAspect: 'backgroundAndMargin',
-                backgroundColor: '#ffffff',
-                margin: '14%',
-                assets: {
-                    ios6AndPriorIcons: false,
-                    ios7AndLaterIcons: false,
-                    precomposedIcons: false,
-                    declareOnlyDefaultIcon: true
-                }
-            },
-            desktopBrowser: {},
-            windows: {
-                pictureAspect: 'noChange',
-                backgroundColor: '#ffc40d',
-                onConflict: 'override',
-                assets: {
-                    windows80Ie10Tile: false,
-                    windows10Ie11EdgeTiles: {
-                        small: false,
-                        medium: true,
-                        big: false,
-                        rectangle: false
-                    }
-                }
-            },
-            androidChrome: {
-                pictureAspect: 'noChange',
-                themeColor: '#ffffff',
-                manifest: {
-                    display: 'standalone',
-                    orientation: 'notSet',
-                    onConflict: 'override',
-                    declared: true
-                },
-                assets: {
-                    legacyIcon: false,
-                    lowResolutionIcons: false
-                }
-            },
-            safariPinnedTab: {
-                pictureAspect: 'silhouette',
-                themeColor: '#5bbad5'
-            }
-        },
-        settings: {
-            scalingAlgorithm: 'Mitchell',
-            errorOnImageTooSmall: false
-        },
-        markupFile: 'favicon.json'
-    });
-});
-
 gulp.task('fonts', function () {
     return gulp.src(fontsIn)
         .pipe(copy(fontsOut, {prefix: 100}))
@@ -141,11 +78,12 @@ gulp.task('fonts', function () {
 
 gulp.task('js', function () {
     return gulp.src(jsIn)
-        .pipe(babel({presets: ['env']}))
+        .pipe(babel())
         .pipe(concat('vendor.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest(jsOut));
 });
 
-gulp.task('default', ['coffee', 'css', 'favicon', 'fonts', 'js'], function () {
-});
+gulp.task('default', gulp.series('coffee', 'css-common', 'css-daily', 'css-nightly', 'fonts', 'js', function (done) {
+    done();
+}));
